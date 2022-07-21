59af03e231057d5e54dbd03df8db139298aa40ce
Minh Nguyen <champion.minh@gmail.com>
2022-07-21 14:55:42 +0700

    Add Antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Input, MenuProps } from "antd";
+import { Breadcrumb, Layout, Menu } from "antd";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Content, Footer, Sider } = Layout;
+
+const items1: MenuProps["items"] = [
+  {
+    key: 1,
+    label: `Login`,
+  },
+  {
+    key: 2,
+    label: `Sign up`,
+  },
+];
+
+const App: React.FC = () => (
+  <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu className="" theme="dark" mode="horizontal" items={items1} />
+    </Header>
+    <Content style={{ padding: "0 50px" }}>
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+        <Content style={{ padding: "0 24px", minHeight: 280 }}>Content</Content>
+      </Layout>
+    </Content>
+    <Footer style={{ textAlign: "center" }}>
+      Ant Design ©2018 Created by Ant UED
+    </Footer>
+  </Layout>
+);
 
 export default App;
